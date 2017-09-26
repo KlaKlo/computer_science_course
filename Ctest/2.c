07a3f0e83ad2360ba0c969f19ba0ac93c81955dd
KlaKlo <klaudia.kloc1@gmail.com>
2017-09-26 17:58:19 +0200

    1

---
+++ Ctest/2.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  2.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  26.09.2017 16:42:36
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (claraloft), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+void hello()
+{
+    printf("hello\n");
+}
+int main()
+{
+    hello();
+    return 0;
+}
+
